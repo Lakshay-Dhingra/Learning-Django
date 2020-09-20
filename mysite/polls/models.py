555a6d0d64384c6500ffc47674e265e9b38f9c59
Lakshay <lakshaydhingra32969@gmail.com>
2020-09-20 12:34:43 +0530

    v2 - views and templates

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta 
 
 # Create your models here.
 class Question(models.Model):
@@ -15,3 +17,6 @@ class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
     votes=models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns=[
-    path("",views.index, name="index")
+    # /polls/
+    path("",views.index, name="index"),
+    # /polls/1/
+    path('<int:question_id>/', views.detail, name="detail"),
+    # /polls/1/results/
+    path('<int:question_id>/results/',views.results, name="results"),
+    # /polls/1/vote/
+    path('<int:question_id>/vote/',views.vote, name="vote")
 ]
